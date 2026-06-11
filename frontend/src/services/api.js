5fd03f006301edd154e523a9e13a07d6b67de1d8
aman dehmiwal <asdehmiwal8888@gmail.com>
2026-06-12 04:35:44 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+  baseURL: "https://inventory-backend-p5vv.onrender.com",
 });
 
 export default api;
